efb3130ccaac84895188b9b691547c9eafb697ca
mrHeinrichh <johnheinrich.fabros@tup.edu.ph>
2024-03-21 02:48:10 +0800

    delete 8 files and update 2 files

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,26 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import Head from 'next/head';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Mr. Heinrich Portfolio',
   description: 'Mr. Heinrich Portfolio',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/logo-16.png" />
+        {/* Add other meta tags if needed */}
+        <meta name="description" content={metadata.description} />
+        <title>{metadata.title}</title>
+        {/* Include Inter font */}
+        <style>{inter.styles}</style>
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
