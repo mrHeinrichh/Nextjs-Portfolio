e686630c54dfc55a1844232d0f209d2ececf6292
Heinric <johnheinrich.fabros@tup.edu.ph>
2024-08-01 11:05:36 +0800

    update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -16,47 +16,55 @@ const HeroSection = () => {
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
-          <span className="text-red-500">
-  Hello, I&apos;m{" "}
-</span>
+            <span className="text-red-500">
+              Hello, I&apos;m{" "}
+            </span>
 
             <br />
             <TypeAnimation
-  sequence={[
-    1000,
-    "John Heinrich Fabros",
-    1000,
-    "Software Developer",
-    1000,
-    "Multimedia Artist",
-    1000,
-  ]}
-  wrapper="span"
-  speed={30}
-  repeat={Infinity}
-  style={{ fontSize: "70px" }} // Adjust the font size as needed
-/>
+              sequence={[
+                1000,
+                "John Heinrich Fabros",
+                1000,
+                "Software Developer",
+                1000,
+                "Multimedia Artist",
+                1000,
+              ]}
+              wrapper="span"
+              speed={30}
+              repeat={Infinity}
+              style={{ fontSize: "70px" }} // Adjust the font size as needed
+            />
 
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-          Taguig City-based creative with a dual passion for Multimedia Arts and Software Engineering. I bring ideas to life visually and build functional applications, blurring the lines between artistry and technical expertise.
+            Taguig City-based creative with a dual passion for Multimedia Arts and Software Engineering. I bring ideas to life visually and build functional applications, blurring the lines between artistry and technical expertise.
           </p>
           <div>
-  <Link
-    href="/#contact"
-    className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-red-500 to-red-600 hover:bg-red-700 text-white"
-  >
-    Contact Me
-  </Link>
-  <Link
-    href="https://raw.githubusercontent.com/mrHeinrichh/Portfolio-cdn/3435af0495dcfbe9826426dbb7385c3ddf013987/pdf/RESUME.pdf"
-    className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-red-500 to-red-600 hover:bg-red-700 text-white mt-3"
-  >
-    <span className="block bg-[#121212] hover:bg-red-700 rounded-full px-5 py-2">
-      Download CV
-    </span>
-  </Link>
-</div>
+            <Link
+              href="/#contact"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-red-500 to-red-600 hover:bg-red-700 text-white"
+            >
+              Contact Me
+            </Link>
+            <Link
+              href="https://raw.githubusercontent.com/mrHeinrichh/Portfolio-cdn/0df7c05f0d803977c7b90242eb4260903c3f02f1/pdf/ITCV.pdf"
+              className="mr-3 px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-red-500 to-red-600 hover:bg-red-700 text-white mt-3"
+            >
+              <span className="block bg-[#121212] hover:bg-red-700 rounded-full px-5 py-2">
+                Download IT CV
+              </span>
+            </Link>
+            <Link
+              href="https://raw.githubusercontent.com/mrHeinrichh/Portfolio-cdn/0df7c05f0d803977c7b90242eb4260903c3f02f1/pdf/MultiMediaCV.pdf"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-red-500 to-red-600 hover:bg-red-700 text-white mt-3"
+            >
+              <span className="block bg-[#121212] hover:bg-red-700 rounded-full px-5 py-2">
+                Download MultiMedia CV
+              </span>
+            </Link>
+          </div>
 
         </motion.div>
         <motion.div
@@ -66,13 +74,13 @@ const HeroSection = () => {
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-          <Image
-  src="https://raw.githubusercontent.com/mrHeinrichh/Portfolio-cdn/main/image/logo.png"
-  alt="logo"
-  className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-  width={300}
-  height={300}
-/>
+            <Image
+              src="https://raw.githubusercontent.com/mrHeinrichh/Portfolio-cdn/main/image/logo.png"
+              alt="logo"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
+            />
 
           </div>
         </motion.div>
